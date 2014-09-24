8ad2c67f4f4c726cdd47ad1bfb78a761f8adb4bc
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-09-24 08:19:20 +0700

    Update mahasiswa.php

--- latihanXML/bab4/mahasiswa.php
+++ latihanXML/bab4/mahasiswa.php
@@ -2,7 +2,7 @@
 
 //1.koneksi database
 $konek = mysql_connect("localhost","root",""));
-$db = mysql_select_db("module_sit");
+$db = mysql_select_db("mahasiswa2");
 
 if ($konek) {
   echo "Berhasil Koneksi ke database. <br/>";
