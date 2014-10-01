33ba9dcadfca0ce8260dffd01ab910d6da6e3411
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-10-01 07:54:16 +0700

    add koneksi.php

---
+++ latihanXML/bab4/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+  // koneksi database
+$link = mysql_connect('127.0.0.1','root','') 
+or die('Could not connect: '.mysql_error());
+mysql_select_db('module_sit') or die('Could not select database');
+
+?>
