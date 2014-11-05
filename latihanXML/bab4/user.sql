214230a1fe7c08f230d0a44c917d3c0ecd183373
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-11-05 07:42:24 +0700

    edit

--- latihanXML/bab4/user.sql
+++ latihanXML/bab4/user.sql
@@ -1,43 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 15, 2014 at 03:53 AM
--- Server version: 5.5.25a
--- PHP Version: 5.4.4
+DROP TABLE IF EXISTS `user`;
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `user`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `username` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(11) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
 
-INSERT INTO `user` (`username`, `password`) VALUES
-('admin', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` (`id`, `username`, `password`)
+VALUES
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(1,'admin','202cb962ac59075b964b07152d234b70');
