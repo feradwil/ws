79050517d8c07d754ff41cd68bbb342b670f34cc
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-11-05 07:41:51 +0700

    edit index.php

--- latihanXML/bab4/index.php
+++ latihanXML/bab4/index.php
@@ -1,40 +1,55 @@
+
 <?php
-	//memulai session
-	session_start();
-	//cek adanya session
-	if (ISSET($_SESSION['username'])){
-	//jika tidak ada session
-	} else {
-	header("location: login.php");
+  error_reporting(E_ALL);
+  ini_set('display_error',1);
+  //memulai session
+  session_start();
+  //cek adanya session
+  if(ISSET($_SESSION['username'])){
+    //jika tidak ada session 
+  } else
+  header("location:login.php");
 ?>
-
-<link href="asser/css/bootstrap.css" rel="stylesheet" type="text/css">
-
-<script type="text/javascript" src="asset/js/bootstrap=dropdown.js"> 
-</script>
-
+  <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="asset/css/navbar-fixed-top.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="asset/js/bootstrap.js"> </script> 
 <body>
-	<div class="navbar navbar fixed-top">
-	<div class="navbar-inner">
-	<div class="row">
-	<div class="span">
-	<a class="brand" href="#">&nbsp E-Learning</a>
-	</div>
-	<div class="nav-collapse">
-	<ul class="nav">
-		<li class="divider"> </li>
-		<li class="dropdown offset7">
-		<a href="#" class="dropdown-toggel" data-toggle="dropdown"></i>
-		<?php 
-		echo "Anda Login sebagai ";
-		echo $_SESSION['username']; ?><b class="caret"></b></a>
-		</li>
-		<li><a href="logout.php"><i class="icon-off"></i>$nbsp; &nbsp; &nbsp; Logout</a></li>
-	</div>
-	</div>
-	</div>
-	<br /><br /><br /><br /><br /><br />
-	<div class="container">
-	<center><h1>Selamat Datang di ELISA Universitas Gadjah Mada</h1></center>
-	</div>
+
+  <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">E-Learning</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar-static-top/">Anda login sebagai <?php echo $_SESSION['username'];?></a></li>
+            <li class="active"><a href="logout.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h3>Selamat Datang di ELISA Universitas Gadjah Mada</h3>
+        <p>Mari berbagi pengetahuan, berkolaborasi dan menjadi produktif dalam kerangka pembelajaran.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Selengkapnya &raquo;</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
 </body>
