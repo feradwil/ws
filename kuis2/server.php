87bf24b68ac4732d5458d6f8101110e0c8bd8eae
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-12-03 08:41:24 +0700

    add

--- kuis2/server.php
+++ kuis2/server.php
@@ -1,13 +1,44 @@
-
-<?
-require_once('nusoap.php');
-$ns = "http://localhost/";
-$server = new soap_server;
-$server->configureWSDL('getCountiesByList', $ns);
-$server->wsdl->schemaTargetNamespace = $ns;
-$server->register('getCountiesByList', array('getCountiesByList' => 'xsd:string'),
-array('return'=>'xsd:string'), $ns);
-function getCountiesByList($getCountiesByList) {
+<?php
+  require_once 'nusoap/lib/nusoap.php';
+  require_once 'adodb/adodb.inc.php';
+  
+  $db = NewADOConnection('mysql');
+ 
+  $server = new nusoap_server();
+  $server->configureWSDL('server','urn:server');
+  $server->wsdl->schemaTargetNamespace = 'urn:server';
+  //register a function that works on server 
+  $server->register('login_ws', 
+    array(
+      'username' => 'xsd:string', 
+      'password'=>'xsd:string'), //parameters 
+      array(
+        'return' => 'xsd: string'
+      ), //output 
+      'urn:server', //namespace 
+      'urn:server#loginServer', //soapaction
+      'rpc', // style 
+      'encoded', // use 
+      'login'
+    ); //description
+      
+    //create function
+    function login_ws($username, $password) { //enkripsi password dengan md5 $password = md5($password);
+      //buat koneksi
+      $db = NewADOConnection('mysql');
+      $password = md5($password);
+      $db -> Connect('localhost','root','','user'); //cek username dan password dari database
+      $sql = $db -> Execute("SELECT * FROM user where username='$username' AND password='$password'");
+      //Cek adanya username dan password di database
+      if ($sql->RecordCount() >= 1) //sama dengan mysql_num_rows pada php biasa
+      {
+        return "Login Berhasil";
+      } else {
+        return "Login gagal";
+      } 
+    }
+	
+	function getCountiesByList($getCountiesByList) {
 if (!$getCountiesByList) {
 return new soap_fault('Client', '', 'Harus ada nilainya!', '');
 }
@@ -34,6 +65,9 @@ return "<b>CountryList: </b>$country<br>
 <b>getCountiesByList: </b>$getCountiesByList<br>
 <b>code: </b>$code<br>;
 }
-$server->service($HTTP_RAW_POST_DATA);
-exit();
+	
+    //create HTTP listener
+    $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : ''; $server->service($HTTP_RAW_POST_DATA);
 ?>
+
+
